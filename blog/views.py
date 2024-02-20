5dae08e390b1a5e039c24d23ee175b9254c0bb26
Saivarun B <saivarun.b97@gmail.com>
2024-02-19 20:54:17 -0500

    feat: saivarun assignment submission

--- blog/views.py
+++ blog/views.py
@@ -2,12 +2,14 @@ from django.shortcuts import render
 
 # Create your views here.
 
+
 def landing_page(request):
     return render(request, "blog/index.html")
 
+
 def posts(request):
-    return render(request, "blog/all-posts.html")
+    return render(request, "blog/all-foods.html")
+
 
 def single_post(request, slug):
-    return render(request, "blog/post-detail.html")
-
+    return render(request, f"blog/includes/{slug}.html")
